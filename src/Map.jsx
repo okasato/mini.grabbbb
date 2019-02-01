21f582837d2ebcdef7950ea3669649a706da4092
Shingo Konnai <k1ckap0l03@gmail.com>
2019-01-31 20:22:41 -0800

    modify innermap

--- src/Map.jsx
+++ src/Map.jsx
@@ -2,15 +2,48 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
  
-const InnerMap = withGoogleMap(({
-  markers  
-}) => {
+const InnerMap = withGoogleMap(props => {
+  console.log('location', props.location)
+  let map = document.getElementsByClassName('map');
+  console.log('map', map)
   const center = {
-    lat: markers.latitude ? markers.latitude : 49.2331455,
-    lng: markers.longitude ? markers.longitude : -123.1188404,
+    lat: props.location.latitude ? props.location.latitude : 49.2331455,
+    lng: props.location.longitude ? props.location.longitude : -123.1188404,
   }
-  // const places = new google.maps.places.PlacesService(center);
-  // console.log(places)
+  const target = new google.maps.Map(map, {
+    center,
+    zoom: 15,
+  });  
+  console.log('target', target)
+
+  const places = new google.maps.places.PlacesService(target);
+  let keyword = 'coffee';
+  places.nearbySearch({
+    location: target.center,
+    radius: '500',
+    name: keyword,
+  }, (results, status) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      // return (
+      //   <GoogleMap
+      //     defaultZoom={12}
+      //     defaultCenter={{ lat: 49.2331455, lng: -123.1188404 }}
+      //     center={center}
+      //   >
+          {/* {results.map(result => {
+            console.log('results', result.geometry.location)
+            return <Marker
+              position={result.geometry.location}
+            />
+          })} */}
+          {/* <Marker position={center}/> */}
+        // </GoogleMap>
+      // ) 
+    } else {
+      console.log(status)
+    }
+  });
+
   return (
     <GoogleMap
       defaultZoom={12}
@@ -24,17 +57,49 @@ const InnerMap = withGoogleMap(({
       })} */}
       <Marker position={center}/>
     </GoogleMap>
-  )} 
+  )
+} 
 );
 
 class Map extends Component{
   render() {
     const { location } = this.props;
+    // console.log('location', location)
+    // let map = document.getElementsByClassName('map');
+    // console.log('map', map)
+    // const center = {
+    //   lat: location.latitude ? location.latitude : 49.2331455,
+    //   lng: location.longitude ? location.longitude : -123.1188404,
+    // }
+    // // const center = new google.maps.LatLng(location.latitude ? location.latitude : 49.2331455, location.longitude ? location.longitude : -123.1188404);
+    // const target = new google.maps.Map(map, {
+    //   center,
+    //   zoom: 15,
+    // });
+    
+    // console.log('target', target)
+    
+    // setTimeout(() => {
+    //   const places = new google.maps.places.PlacesService(target);
+    //   let keyword = 'coffee';
+    //   places.nearbySearch({
+    //     location: target.center,
+    //     radius: '500',
+    //     name: keyword,
+    //   }, (results, status) => {
+    //     if (status === google.maps.places.PlacesServiceStatus.OK) {
+    //       console.log('results', results)
+    //     } else {
+    //       console.log(status)
+    //     }
+    //   });
+    // }, 0)
+
     return (
       <InnerMap
           containerElement={(<div />)}
           mapElement={(<div className='map' style={{ marginTop: 10, height: 400, width: 350}}/>)}
-          markers={location}
+          location={location}
         />
     )
   }
